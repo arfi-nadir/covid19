7f1c4fe83eb523869cf759de9ebdf965ef1183ac
ARFI Nadir <arfi.nadir11@gmail.com>
2020-05-08 06:37:05 +0200

    Updates

---
+++ precache-manifest.486e901794e9eb5cc8c865e872e0f47e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca49ebcc72544597945cdbca4f8dfc81",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "c828b0dfaa801ec9454d",
+    "url": "/covid19/static/css/main.2b74b4b4.chunk.css"
+  },
+  {
+    "revision": "2817b4dba6ed6292340a",
+    "url": "/covid19/static/js/2.cb5f8e26.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.cb5f8e26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c828b0dfaa801ec9454d",
+    "url": "/covid19/static/js/main.a47d1580.chunk.js"
+  },
+  {
+    "revision": "2897f32d4e7477ba260b",
+    "url": "/covid19/static/js/runtime-main.a021e852.js"
+  },
+  {
+    "revision": "02c11d266c0fd25b42a3dc34ebc65efe",
+    "url": "/covid19/static/media/image.02c11d26.png"
+  }
+]);
